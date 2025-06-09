a4be98841a0eef8d2d1727a2de3091b6fe002e5d
bloodnighttw <bbeenn1227@gmail.com>
2025-06-10 04:29:03 +0800

    feat: implement better-auth integration with drizzle ORM and update configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './app/server/db/schema.ts',
+  schema: [
+    './app/server/db/schema.ts',
+    './app/server/db/better-auth.ts'
+  ],
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,

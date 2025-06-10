8e906f12da06e7d7ee98058d112dfc4e9d46609c
bloodnighttw <bbeenn1227@gmail.com>
2025-06-10 21:55:09 +0800

    feat: update redirect path in redirectAuthed function and remove unused home route

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -6,6 +6,16 @@ export const { signIn, signUp, useSession, getSession } = createAuthClient()
 export const redirectAuthed = async () => {
   const session = await getSession();
   if (session.data) {
-    redirect("/"); // Redirect to home if already logged in
+    redirect("/chat"); // Redirect to home if already logged in
   }
+}
+
+// this function checks if the user is authenticated
+// and redirects to the login page if not
+export const requiredAuth = async () => {
+  const session = await getSession();
+  if (!session.data) {
+    throw redirect("/login"); // Redirect to login if not authenticated
+  }
+  return session;
 }
--- app/routes.ts
+++ app/routes.ts
@@ -6,7 +6,6 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
   layout("components/layout/auth.tsx", [
     route("/login", "routes/login.tsx"),
     route("/signup", "routes/signup.tsx"),

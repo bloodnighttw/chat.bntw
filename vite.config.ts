51772a677f21aa3f17b57347318c1ae405af014b
bloodnighttw <bbeenn1227@gmail.com>
2025-06-13 20:46:37 +0800

    Revert "feat: switch Hono server adapter with React Router in Vite config"
    This reverts commit 3d54310d2696b3c8036f0cdfbe898ead83f28a72.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { reactRouterHonoServer } from "react-router-hono-server/dev"; // add this
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import serverAdapter from "hono-react-router-adapter/vite"
-import adapter from "@hono/vite-dev-server/cloudflare"
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    // @ts-expect-error this plugin is not typed yet
+    reactRouterHonoServer(),
     reactRouter(),
-    serverAdapter({
-      adapter,
-      entry: "./app/server/index.ts",
-    }),
     tsconfigPaths(),
   ],
 });
